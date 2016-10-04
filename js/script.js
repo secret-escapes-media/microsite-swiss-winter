dc10f016c7fdeee9ed8afa9ab6b3c7f1202b971e
Hamish Gray <hamishgray@Hamishs-MacBook-Pro.local>
2016-10-04 12:16:14 +0100

    Refining content and design structure

--- js/script.js
+++ js/script.js
@@ -85,64 +85,6 @@
   });
 
 
-/////////////////////////////////////////////////////
-//     360 videos thumbnails
-/////////////////////////////////////////////////////
-
-  // stopped on touch devices, normal link goes to youtube app (if installed)
-  if ( !("ontouchstart" in document.documentElement) ) {
-
-    // Loops through all videos on page
-    $('.js-three-sixty-content').each(function(index, el) {
-      var videoBlock        = $(this),
-          video             = $(this).find('.js-three-sixty-iframe'),
-          videoWrap         = $(this).find('.js-three-sixty-iframe-wrap'),
-          videoSrc          = video.attr('src'),
-          thumbnail         = $(this).find('.js-three-sixty-thumbnail'),
-          playButton        = $(this).find('.js-three-sixty-video'),
-          playingClass      = 'is-playing';
-
-      // hide video, but keep aspect ratio
-      videoWrap.css('visibility', 'hidden');
-
-      // play button event
-      playButton.on('click', function(e) {
-        e.preventDefault();
-
-        // add auto play query to iframe
-        video.attr('src', videoSrc + '&autoplay=1');
-
-        // fade out iframe, show video, remove thumnail elements
-        thumbnail.fadeOut( 175, function() {
-          videoWrap.css('visibility', 'visible');
-          videoBlock.addClass(playingClass);
-          thumbnail.remove();
-        });
-      });
-    });
-  }
-
-///////////////////////////////////////
-//       360 content switcher
-///////////////////////////////////////
-
-// youtube 360 video only works on a certain set of browsers
-// pretty much ie11+, but not win7 ie11 and Safari
-// Modernizer.intl (Internationalization API) seemed to have roughly the same
-// browser support as youtube 360 video
-// broken content is removed from devices that we can detect 360 video doesn't work on
-
-  var threeSixtyContent   = $('.js-three-sixty-content'),
-      threeSixtyVideo     = threeSixtyContent.find('.js-three-sixty-video'),
-      threeSixtyImage     = threeSixtyContent.find('.js-three-sixty-image');
-
-  // checks if content is on page
-  if ( threeSixtyContent.length > 0 ){
-    // if desktop & cant run 360 removes 360 link
-    if ( !("ontouchstart" in document.documentElement) && !(Modernizr.intl) ) {
-      threeSixtyVideo.remove();
-    }
-  }
 
 ///////////////////////////////////////
 //      Modal
